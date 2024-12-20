a93add24522c9d6901c11a9025afce6ebeebce56
Jacob Allen <jallen25@basecampcodingacademy.org>
2024-12-20 12:05:50 -0600

    made changes to animations, added slide cancelling, crouching, general smoothness fixes to animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,43 +1,190 @@
 extends CharacterBody3D
 
+@onready var nek: Node3D = $nek
+@onready var camera_3d: Camera3D = $nek/head/eyes/Camera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.01
-var gravity = 9.8
+@onready var head: Node3D = $nek/head
+@onready var standing_collision_shape: CollisionShape3D = $standing_collision_shape
+@onready var crouching_collision_shape: CollisionShape3D = $crouching_collision_shape
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var eyes: Node3D = $nek/head/eyes
 
-var BOB_FREQ
+var current_speed = 5.0
+const walking_speed = 5.0
+const sprinting_speed = 8.0
+const crouching_speed = 3.0
 
-@onready var head = $Head
-@onready var camera = $Head/Camera3D
+#Sliding vars
+var slide_timer = 0.0
+var slide_timer_max = 1.0
+var slide_vector = Vector2.ZERO
+var slide_speed = 10.0
 
+# States
 
-func ready():
+var walking = false
+var sprinting = false
+var crouching = false
+var free_looking = false
+var sliding = false
+
+#Tilt
+var free_look_tilt_amount = 8.5
+
+const jump_velocity = 4.5
+var lerp_speed = 10.0
+const mouse_sens = 0.25
+var crouching_depth = -0.5
+var direction = Vector3.ZERO
+
+#Head bob
+const head_bobbing_sprinting_speed = 22.0
+const head_bobbing_walking_speed = 14.0
+const head_bobbing_crouching_speed = 10.0
+
+#Intensity of the head bob
+const head_bobbing_sprinting_intensity = 0.2
+const head_bobbing_walking_intensity = 0.1
+const head_bobbing_crouching_intensity = 0.05
+
+var head_bobbing_vector = Vector2.ZERO
+var head_bobbing_index = 0.0
+var head_bobbing_current_intensity = 0.0
+
+func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
 	
-func _unhandled_input(event):
+	
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		
+		if free_looking:
+			nek.rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+			nek.rotation.y = clamp(nek.rotation.y, deg_to_rad(-120), deg_to_rad(120))
+		else:
+			
+			
+			
+			rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		
+		
 func _physics_process(delta: float) -> void:
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	
+	
+	if Input.is_action_pressed("crouch") || sliding:
+		head.position.y = lerp(head.position.y, 1.8 + crouching_depth, delta*lerp_speed)
+		current_speed = crouching_speed
+		standing_collision_shape.disabled = true
+		crouching_collision_shape.disabled = false
+		
+		# Begin slide logic
+		
+		
+		if sprinting && input_dir != Vector2.ZERO:
+			sliding = true
+			slide_timer = slide_timer_max
+			slide_vector = input_dir
+			free_looking = true
+			print("Sliding Enabled")
+		
+		walking = false
+		sprinting = false
+		crouching = true
+		
+		
+	
+	elif !ray_cast_3d.is_colliding():
+		crouching_collision_shape.disabled = true
+		standing_collision_shape.disabled = false
+		head.position.y = lerp(head.position.y, 0.0, delta*lerp_speed)
+		head.position.y = 1.8
+		if Input.is_action_pressed("sprint"):
+			current_speed = sprinting_speed
+			
+			walking = false
+			sprinting = true
+			crouching = false	
+				
+		else:
+			current_speed = walking_speed
+			
+			walking = true
+			sprinting = false
+			crouching = false
+	if Input.is_action_pressed("free_look") || sliding:
+		free_looking = true
+		
+		if sliding:
+			camera_3d.rotation.z = lerp(camera_3d.rotation.z, -deg_to_rad(7.0), delta * lerp_speed		)
+			
+		else:
+			camera_3d.rotation.z = -deg_to_rad(nek.rotation.y * free_look_tilt_amount)
+	else:
+		free_looking = false
+		nek.rotation.y = lerp(nek.rotation.y, 0.0, delta*lerp_speed)
+		camera_3d.rotation.z = lerp(camera_3d.rotation.z, 0.0, delta*lerp_speed)
+		
+	if sliding:
+		slide_timer -= delta
+		if slide_timer <= 0:
+			sliding = false
+			print("Sliding Disabled	")
+			free_looking = false
+		
+	# Handle headbob
+	if sprinting:
+		head_bobbing_current_intensity = head_bobbing_sprinting_intensity
+		head_bobbing_index += head_bobbing_sprinting_speed*delta
+	elif walking:
+		head_bobbing_current_intensity = head_bobbing_walking_intensity
+		head_bobbing_index += head_bobbing_walking_speed*delta
+		
+	elif crouching:
+		head_bobbing_current_intensity = head_bobbing_crouching_intensity
+		head_bobbing_index += head_bobbing_crouching_speed*delta
+		
+	if is_on_floor() && !sliding && input_dir != Vector2.ZERO:
+		head_bobbing_vector.y = sin(head_bobbing_index)
+		head_bobbing_vector.x = sin(head_bobbing_index/2)+0.5
+		
+		eyes.position.y = lerp(eyes.position.y, head_bobbing_vector.y*(head_bobbing_current_intensity/2.0),delta*lerp_speed)
+		eyes.position.x = lerp(eyes.position.x, head_bobbing_vector.x*head_bobbing_current_intensity,delta*lerp_speed)
+	else:
+		eyes.position.y = lerp(eyes.position.y, 0.0,delta*lerp_speed)
+		eyes.position.x = lerp(eyes.position.x	, 0.0,delta*lerp_speed)
+		
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
+		sliding = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "forward", "back")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
+	
+	if sliding:
+		direction = (transform.basis * Vector3(slide_vector.x,0,slide_vector.y)).normalized()
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		
+		velocity.z = direction.z * current_speed
+		
+		if sliding:
+			velocity.x = direction.x * (slide_timer + 0.1) * slide_speed
+			velocity.z = direction.z * (slide_timer + 0.1)	 * slide_speed
 	else:
-		velocity.x = 0
-		velocity.z = 0
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
